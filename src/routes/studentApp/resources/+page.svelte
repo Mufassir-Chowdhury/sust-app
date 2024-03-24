51101078de73318cf3332ccc775e3c26533533d8
2019331013 <asanulhaquekiron@gmail.com>
2024-03-24 13:21:15 +0600

    Add resources page with navigation links

---
+++ src/routes/studentApp/resources/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	const items = [
+		{ id: 1, name: 'Notes', url: '/studentApp/resources/notes' },
+		{ id: 2, name: 'Videos', url: '/studentApp/resources/videos' },
+		{ id: 3, name: 'Questions', url: '/studentApp/resources/questions' },
+		{ id: 4, name: 'item 4', url: '/studentApp/resources/item4' },
+		{ id: 5, name: 'item 5', url: '/studentApp/resources/item5' }
+	];
+</script>
+
+<div class="flex items-center justify-center my-auto">
+	<div
+		class="container grid grid-cols-3 md:grid-cols-4 gap-6 p-4 items-center justify-center my-auto"
+	>
+		{#each items as item}
+			<a
+				href={item.url}
+				class="card cursor-pointer card-hover variant-soft-surface flex flex-col p-8 items-center justify-center gap-4"
+			>
+				<i class="fi fi-tr-clipboard" />
+				{item.name}
+			</a>
+		{/each}
+	</div>
+</div>
